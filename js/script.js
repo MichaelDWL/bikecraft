0fb48aa943f58a799c39a052022578de4cd07ba8
Michael_DWL <michaeldouglas.devs@gmail.com>
2026-02-02 21:20:07 -0300

    add galeria function

--- js/script.js
+++ js/script.js
@@ -24,3 +24,26 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+console.log(galeria, galeriaContainer);
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
